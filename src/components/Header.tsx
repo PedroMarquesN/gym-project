a4c5824566fb5b9b528c44958b4f663a6b241447
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-04-10 18:05:40 -0300

    Feat:working in nav and mobile nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,21 +3,55 @@ import Nav from "@/components/Nav";
 import MobileNav from "@/components/MobileNav";
 import Link from "next/link";
 import Image from "next/image";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {MdMenu} from "react-icons/md";
 
 export default function Header() {
     const [headerActive, setHeaderActive] = useState(false)
+    const [openNav, setOpenNav] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setHeaderActive(window.scrollY > 50)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, []);
     return (
-        <header className="fixed left-0 right-0 w-full  bg-primary-200 h-[100px]">
+        <header
+            className={`
+                ${headerActive ? 'h-[100px]' : 'h-[124px]'}
+                fixed top-0 w-full max-w-[1920px]
+                bg-primary-200 h-[100px] transition-all z-50`}>
             <div className="container mx-auto h-full flex items-center justify-between">
                 {/*Logo */}
                 <Link href=''>
                     <Image src={'/assets/img/logo.png'} alt="Imagem da logo" width={117} height={55}/>
                 </Link>
                 {/*mobile nav*/}
-                <MobileNav containerStyle="xl:hidden text-white" />
+                <MobileNav
+                    containerStyle={`
+                        ${headerActive ? 'top-[90px]' :'top-[124px]'}
+                        ${openNav ? 'max-h-max pt-8 pb-10 border-t border-white/10'
+                        :'max-h-0 pt-0 pb-0 overflow-hidden border-white/0'}
+                        flex flex-col text-center gap-8 xl:hidden text-white
+                        fixed bg-primary-200 w-full left-0 text-base uppercase
+                        font-medium transition-all
+                    `}
+                />
                 {/*desktop nav*/}
                 <Nav containerStyles='flex gap-4 hidden xl:flex text-white' />
+                {/*hide/open menu button */}
+                <div>
+                    <button onClick={() => setOpenNav(!openNav)} className={`text-white xl:hidden`}>
+                        <MdMenu  className={`text-xl`}/>
+                    </button>
+                </div>
+
             </div>
         </header>
     )
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,7 +1,41 @@
+"use client"
+import {Link as ScrollLink} from 'react-scroll'
+import {useMediaQuery} from "react-responsive";
+
+
+const links = [
+    {name:'home', target:'home', offset: -100},
+    {name:'about', target:'about', offset: -80},
+    {name:'class', target:'class', offset: -80},
+    {name:'team', target:'team', offset: 0},
+    {name:'prices', target:'prices', offset: -40},
+    {name:'testimonial', target:'testimonial', offset: 0},
+    {name:'blog', target:'blog', offset: 0},
+    {name:'contact', target:'contact', offset: 0}
+]
+
+
 export default function MobileNav({containerStyle}:{containerStyle: string}) {
+    const isMobile = useMediaQuery({
+        query: '(max-width: 640px)'
+    })
     return (
         <nav className={`${containerStyle}`}>
-            Mobile
+                {links.map((link, index) => {
+                    return (
+                        <ScrollLink
+                            className="cursor-pointer  hover:text-accent transition-all"
+                            key={index}
+                            to={link.target}
+                            smooth
+                            spy
+                            activeClass={`${!isMobile && 'active'}`}
+                            offset={link.offset}
+                        >
+                            {link.name}
+                        </ScrollLink>
+                    )
+                })}
         </nav>
     )
 }
