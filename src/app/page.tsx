331697463958c32954e1197192ca046f23d8e64e
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-04-10 14:33:51 -0300

    Feat: create components , and config tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
       <main>
-        HomePage
+        <Hero />
+        <div className="h-[3000px]"></div>
       </main>
   );
 }
