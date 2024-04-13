394de76999a6c607b7d230060f556db5e8dd88b1
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-04-13 13:34:21 -0300

    Feat:working in about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,57 @@
+'use client'
+import {FaDumbbell, FaUsers} from "react-icons/fa";
+import {IoIosPricetags} from "react-icons/io";
+
+// @ts-ignore
+const featured = [
+    {
+        icon: <FaUsers/>,
+        title: "Treinadores qualificados",
+        subtitle: "Treinadores qualificados e com experiência"
+    },
+    {
+        icon: <FaDumbbell/>,
+        title: "Equipamentos modernos",
+        subtitle: "Equipamentos modernos e de última geração"
+    },
+    {
+        icon: <IoIosPricetags />,
+        title: "Preços acessíveis",
+        subtitle: 'Preços acessíveis para todos os bolsos',
+    }
+];
+
 export default function About() {
     return (
-        <div className="h-screen bg-yellow-300" id='about'>About</div>
+        <section className="pt-8 pb-14 lg:pt-16 lg:pb-28" id='about'>
+            <div className="container mx-auto">
+                <div className={`flex flex-col items-center gap-2 mb-8`}>
+                    <h1 className={`text-black text-center`}>Sobre Nós</h1>
+                    <p className={`max-w-[600px] mx-auto text-center`}>
+                        A Academia Fitness é um espaço dedicado ao treino e à saúde. Temos uma equipa de treinadores
+                        qualificados e equipamentos modernos para te ajudar a alcançar os teus objetivos.
+                    </p>
+                </div>
+            </div>
+
+            {/*featured items*/}
+            <div className={` grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 mb-16`}>
+                {featured.map((item, index) => {
+                    return (
+                        <div className={`flex flex-col justify-center items-center gap-4 border p-10`} key={index}>
+                            <div className={`text-4xl bg-primary-300 text-white w-[80px] h-[80px]
+                                rounded-full flex items-center justify-center
+                            `}>{item.icon}</div>
+                            <div className={` flex flex-col justify-center items-center gap-2 text-center`}>
+                                <h2 className={`font-bold text-accent`}>{item.title}</h2>
+                                <p>{item.subtitle}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+            {/*achievements*/}
+
+        </section>
     )
 }
--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -45,22 +45,42 @@ export default function HeroSlider() {
                     </div>
                 </div>
             </SwiperSlide>
-            <SwiperSlide>
+            <SwiperSlide >
                 <div className={`h-full flex justify-end pt-64`}>
+                    {/*retirei o lg:max-w-[700px] fazer teste em outra tela*/}
                     <div className={`flex flex-col items-center lg:items-start `}>
-                        <h1 className={`uppercase text-center lg:text-left mb-2`}>
-                            Aqui o<span> seu esforço</span> tem recompensa
-                        </h1>
-                        <p className={`text-white italic text-center lg:text-left mb-4 `}>
-                            venha fazer parte do nosso time
-                        </p>
-                        <CustomButton
-                            text='Comece já'
-                            containerStyles='w-[196px] h-[62px] mt-2 lg:mx-auto'
-                        />
+                        <motion.h1
+                            variants={fadeIn('up',0.4)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{once: false,amount: 0.2}}
+                            className={`uppercase text-center lg:text-left mb-2`}>
+                            Belos planos para<span> um belo corpo</span>
+                        </motion.h1>
+                        <motion.p
+                            variants={fadeIn('up',0.6)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{once: false,amount: 0.2}}
+                            className={`text-white italic text-center lg:text-left mb-4 `}>
+                            Venha conhecer a melhor academia do Brasil.
+                        </motion.p>
+                        <motion.div
+                            variants={fadeIn('up',0.8)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{once: false,amount: 0.2}}
+                        >
+                            <CustomButton
+                                text='Comece já'
+                                containerStyles='w-[196px] h-[62px] mt-2 lg:mx-auto'
+                            />
+                        </motion.div>
+
                     </div>
                 </div>
             </SwiperSlide>
+
             {/*swiper nav buttons */}
             <SwiperNavButtons
                 containerStyles='absolute bottom-2 lg:bottom-0 right-0  h-[130px] w-full lg:w-[700px]
